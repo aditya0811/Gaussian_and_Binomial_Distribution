6efaf67239c8bb0a3d6e815fd20e60a93b1794fa
Aditya Mahajan <aditya.m.1997@gmail.com>
2020-12-12 12:30:27 +0530

    Add files via upload

---
+++ vzerodotoneDistribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vzerodotoneDistribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vzerodotoneDistribution'],
+      zip_safe=False)
